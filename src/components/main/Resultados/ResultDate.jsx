de117a216114611da1db4fcfa5896c89554c653b
Exequiel <exe2016gerez@gmail.com>
2023-01-21 21:03:42 -0300

    agregando spinner de bootstrap

--- src/components/main/Resultados/ResultDate.jsx
+++ src/components/main/Resultados/ResultDate.jsx
@@ -1,45 +1,58 @@
- import { useEffect, useState } from "react";
- import { firebaseBuscar } from "../../../utils/firebase";
+import { useEffect, useState } from "react";
+import { firebaseBuscar } from "../../../utils/firebase";
 
- import "./ResultDate.styles.css";
+import "./ResultDate.styles.css";
+import "../../../bootstrap/bootstrap-spinner.css"
 
- const ResultDate = () => {
-     const [usuarios, setUsuarios] = useState([]);
+const ResultDate = () => {
+    const [usuarios, setUsuarios] = useState([]);
 
-     useEffect(() => {
-         buscarUsuarios();
-     },[]);
+    useEffect(() => {
+        buscarUsuarios();
+    }, []);
 
-     async function buscarUsuarios (){
-         let resultado = await firebaseBuscar("usuarios");
-         setUsuarios(resultado);
-     }
+    async function buscarUsuarios() {
+        let resultado = await firebaseBuscar("usuarios");
+        setUsuarios(resultado);
+    }
 
-     return ( 
-         <div className="result">
-             <h3>Datos</h3>
-             <table border="1px solid #ccc">
-                 <thead>
-                     <tr>
-                         <td>Nombre completo</td>
-                         <td>Correo electrónico</td>
-                         <td>Fecha de nacimiento</td>
-                         <td>País de origen</td>
-                     </tr>
-                 </thead>
-                 <tbody>
-                    {usuarios? usuarios.map((usuario) => (
+    return (
+        <div className="result">
+            <h3>Datos</h3>
+            <table border="1px solid #ccc">
+                <thead>
+                    <tr>
+                        <td>Nombre completo</td>
+                        <td>Correo electrónico</td>
+                        <td>Fecha de nacimiento</td>
+                        <td>País de origen</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {!usuarios && (
+                        <div className="container">
+                            <button className="btn btn-primary" type="button" disabled>
+                                <span
+                                    className="spinner-border spinner-border-sm"
+                                    role="status"
+                                    aria-hidden="true"
+                                ></span>
+                                Cargando...
+                            </button>
+                        </div>
+                    )}
+                    {usuarios && usuarios.map((usuario) => (
                         <tr key={usuario.id}>
                             <td>{usuario.full_name}</td>
                             <td>{usuario.email}</td>
                             <td>{usuario.birth_date}</td>
                             <td>{usuario.country_of_origin}</td>
                         </tr>
-                    )): <td>No hay datos que mostrar</td>}
-                 </tbody>
+                    ))}
+                </tbody>
             </table>
         </div>
-     );
+    );
 }
- 
+
 export default ResultDate; 
